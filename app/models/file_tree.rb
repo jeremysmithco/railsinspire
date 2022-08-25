0190c723e474f8b024961f5fd157922935da5131
Jeremy Smith <jeremy@jeremysmith.co>
2022-08-24 23:30:46 -0400

    Update FileTree and FileTreeComponent to pass file object through for links

--- app/models/file_tree.rb
+++ app/models/file_tree.rb
@@ -1,23 +1,23 @@
 class FileTree
-  def initialize(paths)
-    @paths = paths
+  def initialize(files)
+    @files = files
   end
 
   def tree
-    paths.sort
-         .map { |path| path_hash(path.split("/")) }
+    files.sort
+         .map { |file| path_hash(file.path.split("/"), file) }
          .reduce({}) { |full, path| full.deep_merge(path) }
   end
 
   private
 
-  attr_reader :paths
+  attr_reader :files
 
-  def path_hash(fragments)
+  def path_hash(fragments, file)
     if fragments.size == 1
-      { fragments[0] => fragments[0] }
+      { fragments[0] => file }
     else
-      { fragments[0] => path_hash(fragments[1..-1]) }
+      { fragments[0] => path_hash(fragments[1..-1], file) }
     end
   end
 end
