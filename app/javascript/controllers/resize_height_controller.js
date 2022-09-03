60ff3f48cc2e3dbcd2b70c27eec805db880b9941
Jeremy Smith <jeremy@jeremysmith.co>
2022-09-03 18:38:08 -0400

    Update resize-height controller

--- app/javascript/controllers/resize_height_controller.js
+++ app/javascript/controllers/resize_height_controller.js
@@ -1,21 +1,34 @@
 import { Controller } from "@hotwired/stimulus";
 import { FetchRequest } from "@rails/request.js";
+import { useResize, useDebounce } from "stimulus-use";
 
 export default class extends Controller {
-  static values = { url: String, setting: String, height: String }
+  static values = { url: String, setting: String, currentHeight: String }
+  static debounces = ["resize"];
 
-  update(event) {
-    if (this.element != event.target) return;
-    const height = parseInt(this.element.style.height);
-    if (isNaN(height)) return;
+  connect() {
+    useResize(this);
+    useDebounce(this);
+  }
 
+  resize({ height }) {
+    console.log(height);
+    console.log(this.currentHeightValue);
+
+    if (height == this.currentHeightValue) return;
+
+    this.update(height);
+  }
+
+  async update(height) {
     let formData = new FormData();
     formData.append("height", height);
     formData.append("setting", this.settingValue);
 
-    const request = new FetchRequest(
-      "patch", this.urlValue, { body: formData }
-    );
-    request.perform();
+    const request = new FetchRequest("patch", this.urlValue, { body: formData });
+    const response = await request.perform();
+    if (response.ok) {
+      this.currentHeightValue = height;
+    }
   }
 }
