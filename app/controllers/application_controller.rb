9734396eeb84fc8323a896dc3397ad2ea9cb0c1d
Jeremy Smith <jeremy@jeremysmith.co>
2022-09-03 23:41:32 -0400

    Add pundit for proper authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  include Pundit::Authorization
   before_action :authenticate_user!
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :samples
+
+  def admin?
+    false
+  end
 end
