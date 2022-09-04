642f7f0fcd4f8b81b8418357ce725ea108a518dd
Jeremy Smith <jeremy@jeremysmith.co>
2022-09-04 00:28:41 -0400

    Add ability to create and edit categories, add validations (#6)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,4 +9,42 @@ class CategoriesController < ApplicationController
     @category = Category.find_by(slug: params[:id])
     authorize @category
   end
+
+  def new
+    @category = Category.new
+    authorize @category
+  end
+
+  def create
+    @category = Category.new(category_params)
+    authorize @category
+
+    if @category.save
+      redirect_to category_path(@category.slug)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+    authorize @category
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    authorize @category
+
+    if @category.update(category_params)
+      redirect_to category_path(@category.slug)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :slug, :description)
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :samples
+
+  validates :name, presence: true, uniqueness: true
+  validates :slug, presence: true, uniqueness: true, slug: true
 end
