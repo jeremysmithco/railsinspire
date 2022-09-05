39e064fdc0735c90f0200ba5353dc2c2d4a39fe6
Jeremy Smith <jeremy@jeremysmith.co>
2022-09-05 12:47:18 -0400

    Fix bug where defaults were 0, set min/max width on contents

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,14 +46,14 @@ module ApplicationHelper
   end
 
   def menu_width_or_default
-    cookies[:menu_width].presence.to_i || 250
+    cookies[:menu_width].presence || 250
   end
 
   def contents_width_or_default
-    cookies[:contents_width].presence.to_i || 650
+    cookies[:contents_width].presence || 650
   end
 
   def editor_height_or_default
-    cookies[:editor_height].presence.to_i || 650
+    cookies[:editor_height].presence || 650
   end
 end
