2ccf71b2c8db27bcab83bd75d9bf564d4b250571
Jeremy Smith <jeremy@jeremysmith.co>
2022-09-08 22:32:04 -0400

    Add custom error pages

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Railsinspire
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.exceptions_app = self.routes
 
     # Configuration for the application, engines, and railties goes here.
     #
