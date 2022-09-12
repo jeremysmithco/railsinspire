0f8dd9286d5dacab1e0a106fb315f0d6b7a9fc46
Jeremy Smith <jeremy@jeremysmith.co>
2022-09-11 22:08:52 -0400

    Set order on has_many :sample_files association

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -3,7 +3,7 @@ class Sample < ApplicationRecord
 
   belongs_to :category
   belongs_to :user
-  has_many :sample_files
+  has_many :sample_files, -> { order(:path) }
   accepts_nested_attributes_for :sample_files, reject_if: :all_blank, allow_destroy: true
 
   validates :title, presence: true
