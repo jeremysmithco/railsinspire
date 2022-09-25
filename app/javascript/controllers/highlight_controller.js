16ded711be257d3d69f5e00d3666f790093b20c2
Jeremy Smith <jeremy@jeremysmith.co>
2022-09-25 15:05:09 -0400

    Remove Highlight.js

--- app/javascript/controllers/highlight_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-
-export default class extends Controller {
-  connect() {
-    this.element.querySelectorAll('pre').forEach((block) => {
-      hljs.highlightElement(block);
-    });
-  }
-}
