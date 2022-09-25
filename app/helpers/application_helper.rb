1294508f55275eed74450a3b0c22225933f9c97c
Jeremy Smith <jeremy@jeremysmith.co>
2022-09-25 12:50:18 -0400

    Add SVGInline formatter, use clip-path for clipping text to editor

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 require 'default_renderer'
 require 'preview_renderer'
 require 'one_dark'
+require 'svg_inline'
 
 module ApplicationHelper
   def syntax_higlight(content, path)
-    formatter = Rouge::Formatters::HTMLInline.new("one-dark")
+    formatter = Rouge::Formatters::SVGInline.new("one-dark")
     lexer = Rouge::Lexer.guess(filename: path)
     formatter.format(lexer.lex(content))
   end
