05ed8b10cc8e746856779300357df34b9213bc68
Jeremy Smith <jeremy@jeremysmith.co>
2023-01-14 11:50:54 -0500

    Revert "Set test queue_adapter"
    This reverts commit 9936b11020ccb0d546625d46e281e633ba872d2e.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -36,8 +36,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory.
   config.active_storage.service = :test
 
-  config.active_job.queue_adapter = :test
-
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
