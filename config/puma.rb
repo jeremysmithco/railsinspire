98a662996f3a37017c0c6a288e506cd939f9d823
Jeremy Smith <jeremy@jeremysmith.co>
2023-01-14 12:33:55 -0500

    Configure puma for sidekiq embedded mode

--- config/puma.rb
+++ config/puma.rb
@@ -29,15 +29,29 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
-#
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+workers ENV.fetch("WEB_CONCURRENCY") { 1 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
-#
-# preload_app!
+
+preload_app!
+
+x = nil
+on_worker_boot do
+  x = Sidekiq.configure_embed do |config|
+    # config.logger.level = Logger::DEBUG
+    config.queues = %w[default]
+    config.concurrency = 1
+  end
+  x.run
+end
+
+on_worker_shutdown do
+  x&.stop
+end
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
